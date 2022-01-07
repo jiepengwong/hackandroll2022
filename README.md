6834d846dd0dff61e2f930f7dd7db334c1838616
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2022-01-07 11:13:50 +0800

    Starting template for telegram bot

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Splitter expense bot by team superidoldexiaorong
 
 # Team
 Wong Jie Peng, Lee Chun Yang, Seth Teng Shann, Sim Wee Kee
+
+# Installation
+
+pip install
---
+++ main.py
@@ -0,0 +1,21 @@
+# Import python telegram bot 
+import telebot
+
+# Import Api KEY
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+# Creation of telegram bot
+bot = telebot.TeleBot(API_KEY)
+
+# Start command
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Welcome to SplitYourMoneyLahBot")
+
+
+
+# Check for messages
+bot.polling()
