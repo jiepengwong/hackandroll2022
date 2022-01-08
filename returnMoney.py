3c82dc64b44d166b236794ba64a03b334fed24d5
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2022-01-08 09:27:45 +0800

    Functiona splitting

--- returnMoney.py
+++ returnMoney.py
@@ -1,41 +1,59 @@
-# ChaT iD  -781874170
+# Import python telegram bot  
+import telebot 
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+ 
+# Import Api KEY 
+from dotenv import load_dotenv 
+import os 
 
+load_dotenv() 
+API_KEY = os.getenv('API_KEY') 
+# Creation of telegram bot 
+bot = telebot.TeleBot(API_KEY) 
 
-# Import python telegram bot 
-import telebot
+people = ["a", "b"]
+ 
+@bot.message_handler(commands=['exit'])
+def exit(message):
+    bot.send_message(message.chat.id, "Exit") 
 
-from telebot.callback_data import CallbackData, CallbackDataFilter
-from telebot import types
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-from telebot import apihelper
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-apihelper.ENABLE_MIDDLEWARE = True
+#Start command 
+@bot.message_handler(commands=['help']) 
+def send_welcome(message): 
+    bot.reply_to(message, "/Start to start")
 
+@bot.message_handler(commands=['start']) 
+def send_welcome(message): 
+    bot.reply_to(message, "Welcome to SplitYourMoneyLahBot") 
+    bot.reply_to(message, "Choose one of the options", reply_markup=displayOptions()) 
 
-# Import Api KEY
-from dotenv import load_dotenv
-import os
+ 
+#display Options 
+def displayOptions(): 
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.row("/createExpense")
+    markup.row("/payments")
+   
+    return markup 
+ 
+@bot.callback_query_handler(func=lambda call: True) 
+def callback_query(call): 
+    if call.data == "create_expense": 
+        startCreateExpense(call.message) 
+    elif call.data == "return_money": 
+        msg = bot.send_message(call.message.chat.id, "Type /pay to start")
 
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
-# Creation of telegram bot
-bot = telebot.TeleBot(API_KEY)
-import csv
+        bot.answer_callback_query(call.id, msg)
+        
 
-
-# Data to store the people that own people money
-
-people = ["a","b"]
-
-#  After "Return money" is clicked
-# Simulate using /pay
-
-@bot.message_handler(commands=['pay'])
+temp = {}
+# Return Money
+@bot.message_handler(commands=['payments'])
 def payment(message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
     for i in people:
-        text = f"/returnMon {i}"
+        text = f"/returnMoney {i}"
         markup.row(text)
     bot.reply_to(message, 
     """Welcome to the payments section, 
@@ -64,116 +82,112 @@ def returnMon(message):
 
 
 
+# Create expense
+
+@bot.message_handler(commands=['createExpense'])
+def startCreateExpense(message):
+    msg = bot.send_message(message.chat.id, "Enter Name of Expense") 
+    bot.register_next_step_handler(msg, handleExpenseName)
 
 
+def printSummary(message):
+    print(temp)
+    emptyString = ""
+    for key,value in temp.items():
+        print(key)
+        print(value)
+        emptyString += f"{key}: {value} \n"
+        # if(key == "whoOweYou"):
+        #     strWhoOweYou = ",".join(temp[key])
+        #     msg = bot.send_message(message.chat.id, key +" "+strWhoOweYou)
+        # elif(key == "totalAmtAfterTax"):
+        #     msg = bot.send_message(message.chat.id, key +" "+str(temp[key]))
+        
+        # elif(type(temp[key]) == int):
+        #     msg = bot.send_message(message.chat.id, key +" "+str(temp[key]))
+        # else:
+        #     print(key,temp[key])
+
+    bot.send_message(message.chat.id,emptyString)
+        
+
+        
+
+def handleExpenseName(message): #input expense name 
+    temp["expenseName"] = message.text
+    msg = bot.send_message(message.chat.id, "Enter Expense Amount Before Taxes") 
+    bot.register_next_step_handler(msg, handleInputAmt)
+
+def handleInputAmt(message): 
+    temp["total"] = int(message.text)
+    msg = bot.send_message(message.chat.id, "Enter who owe you (eg @cy, @jp") 
+    bot.register_next_step_handler(msg, handleWhoOweYou)
+
+
+def handleWhoOweYou(message): 
+    listOfWhoOweYou = message.text.split()
+    temp["whoOweYou"] = listOfWhoOweYou
+    temp["totalNumofPpl"] = len(listOfWhoOweYou) + 1
+    splitMethodOptions(message)
+
+def splitMethodOptions(message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.row("Evenly")
+    markup.row("Manually")
+    msg = bot.send_message(message.chat.id,"do you want to split evenly or manually?", reply_markup=markup)
+    bot.register_next_step_handler(msg, handleSplitMethod)
+
+def handleSplitMethod(message):
+    temp["splitMethod"] = message.text
+
+    if temp["splitMethod"] == "Evenly":
+        temp["payableAmount"] = temp["total"] / (len(temp['whoOweYou']) + 1)
+        msg = bot.send_message(message.chat.id, "Summary")
+        printSummary(message)
+        return
+
+
+    if temp["splitMethod"] == "Manually":
+        
+        msg = bot.send_message(message.chat.id, """
+        You have selected manualy, please type the specific amount people owe you
+        (E.g jp:50,cy:50)
+        """)
+        bot.register_next_step_handler(msg,handleManualSplit)
+
+def handleManualSplit(message):
+    temp["payableAmount"] = {}
+
+    test = message.text 
+    testArray = message.text.split(",")
+
+
+    for person in testArray:
+        details = person.split(":")
+        temp["payableAmount"][details[0]] = details[1]
+
+    msg = bot.send_message(message.chat.id, "Summary")
+    printSummary(message)
+
+
+
+# def handleGSTandTax(message):
+#     temp["gstAndTax"] = int(message.text)
+#     totalamtAfterTax = temp["totalAmtBeforeTax"]
+#     multipler = 1 + temp["gstAndTax"]/100
+#     totalamtAfterTax = round(temp["totalAmtBeforeTax"] *  multipler, 2) 
+#     temp["totalAmtAfterTax"] = totalamtAfterTax
+#     handleSplitBills(message)
     
-        
 
-
-        
-
-    
-
-	
+def handleSplitBills(message):
+    print("hi i am rendered")
+    printSummary(message)
 
 
 
 
 
-# def command_time(m):
-#     text = m.text
-#     chatid = m.chat.id
-#     print(text) 
-#     a = checkFormat(text)
-#     print(a)
-#     if a != True:
-#         bot.send_message(chatid, "Wrong Format!")
-#         return
-#     else:
-#         appt.append(text)
-#         msg = bot.send_message(chatid, "What is the time of the appointment? Example:1200")
-#         bot.register_next_step_handler(msg, command_des)
 
 
-
-
-# Start command
-# PAY command select individual to pay to
-# selectedPerson = ['jpoggers','cygay']
-# @bot.message_handler(content_types=['image'])
-# def photoValidation(person):
-#     print(person)
-#     return "hi"
-
-# # /pay @jpoggers
-# @bot.message_handler(commands=['pay'])
-# def lol(message):
-#     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-#             # From database now is just dummy data
-#     for person in selectedPerson:
-#         markup.add(person)
-#     msg = bot.reply_to(message, 'These are the people which you owe money to Jppoggers - 25, cy - 50, selected 1 which you wish to pay ')
-#     bot.register_next_step_handler(msg, process_choose_step)
-
-
-
-# @bot.message_handler(func=lambda m: True)
-# def process_choose_step(message):
-
-
-#         chat_id = message.chat.id
-#         msg = message.text
-#         bot.send_message(message, 'Nice to meet you ')
-
-
-            # if (msg== u'jpoggers') or (msg == u'cygay'):
-
-            #     bot.send_message(message, 'Nice to meet you ')
-
-     
-        
-
-
-
-# def process_select_user_step(message):
-    
-
-
-
-
-# def gen_markup():
-#     markup = InlineKeyboardMarkup()
-#     markup.row_width = 2
-
-#     for people in selectedPerson:
-#          markup.add(InlineKeyboardButton(people, callback_data=people))    
-        
-#     return markup
-# #callback function?
-
-
-# @bot.callback_query_handler(func=lambda call: True)
-# def callback_query(call):
-#     for people in selectedPerson:
-
-#         if call.data == people:
-#             msg = bot.reply_to(cal, 'Please send a picture to confirm payment')
-#             bot.answer_callback_query(call.id, msg)
-#             bot.register_next_step_handler(msg, process_picture_step)
-#             break
-#         elif call.data == "cb_no":
-#             bot.answer_callback_query(call.id, "Answer is No")
-
-
-
-# def process_picture_step(message):
-#     try:
-#          chat_id = message.chat.id
-#          name = message.text
-#          bot.send_message(message.chat, 'paid')
-#     except:
-#         bot.reply_to(message, "lol")
-
-
-
-bot.infinity_polling()
+bot.polling()
